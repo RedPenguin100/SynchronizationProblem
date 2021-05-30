e1c95fdea8a81f3efe2767039b098d41c0105d16
Michael Kovaliov <michaelkovaliov97@gmail.com>
2021-05-30 22:35:00 +0300

    first commit

---
+++ exer1.py
@@ -0,0 +1,162 @@
+import random
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_rotation_x(theta):
+    return np.array([[1, 0, 0],
+                     [0, np.cos(theta), -np.sin(theta)],
+                     [0, np.sin(theta), np.cos(theta)]])
+
+
+def get_rotation_y(theta):
+    return np.array([[np.cos(theta), 0, np.sin(theta)],
+                     [0, 1, 0],
+                     [-np.sin(theta), 0, np.cos(theta)]])
+
+
+def get_rotation_z(theta):
+    return np.array([[np.cos(theta), -np.sin(theta), 0],
+                     [np.sin(theta), np.cos(theta), 0],
+                     [0, 0, 1]])
+
+
+def get_random_rotation():
+    theta1, theta2, theta3 = np.random.uniform(0, 2 * np.pi, 3)
+    return get_rotation_x(theta1) @ get_rotation_y(theta2) @ get_rotation_z(theta3)
+
+
+def get_so_projection(X, n=3):
+    U, sigma, Vt = np.linalg.svd(X)
+    # The dtype conversion is absolutely necessary because
+    # the numerical error is too large otherwise.
+    S = np.zeros((n, n), dtype='complex128')
+    np.fill_diagonal(S, 1)
+    S[-1][-1] = np.linalg.det(U @ Vt)
+    # The conj() might not be necessary for the real matrices
+    # but is absolutely-must for the complex matrices.
+    ret = (U.conj() @ S) @ Vt.conj()
+    return ret
+
+
+def _get_minimizer(expected, actual):
+    n, d, d2 = expected.shape
+    expected = expected.reshape((n * d, d))
+    actual = actual.reshape((n * d, d))
+
+    return get_so_projection(actual.T @ expected, d)
+
+
+def get_error(expected, actual, dim=3):
+    # Validation logic
+    assert expected.shape == actual.shape, "Dimension mismatch!"
+    assert len(expected.shape) == 3
+    n, d, d2 = expected.shape
+    assert d == dim
+    assert d2 == dim
+
+    # Error retrieving logic
+    Q = _get_minimizer(expected, actual)
+    error = 0
+    for i in range(n):
+        # The addition of `conj` to expected[i] is crucial for the complex case.
+        error += np.linalg.norm(expected[i].conj() - actual[i] @ Q) ** 2
+
+    return error
+
+
+def solve_sync_with_spectral(data, d=3):
+    # Assertions
+    assert len(data.shape) == 2
+    nd, nd2 = data.shape
+    assert nd == nd2
+    n = nd // d
+    assert n * d == nd
+
+    for i in range(n):
+        assert np.isclose(data[i][i], 1)
+
+    # Actual derivation
+    w, v = np.linalg.eig(data)
+
+    v_args = np.argwhere(np.isclose(n, w))
+    V_hat = v[:, v_args].reshape((n, d, d))
+
+    R_hat = np.copy(V_hat)
+    for i in range(n):
+        R_hat[i] = get_so_projection(V_hat[i], d)
+    return R_hat
+
+
+def truly_random_so_matrix(n):
+    """
+    :param n: dimension
+    """
+    z = (np.random.randn(n, n) + 1j * np.random.randn(n, n)) / np.sqrt(2.0)
+    Q, R = np.linalg.qr(z)
+    D = np.diagonal(R)
+    normalizing_matrix = D / np.absolute(D)
+    Q_tag = np.multiply(Q, normalizing_matrix)
+    Q_tag = Q_tag.astype('complex_')
+    return get_so_projection(Q_tag, n)
+
+
+def haar_measure_sampling_evidence(n):
+    """
+    Choose a high dimension(~1000+) so that it is more visible.
+    :note: computation might take a while.
+    """
+    M = truly_random_so_matrix(n)
+    w, v = np.linalg.eig(M)
+    count, bins, ignored = plt.hist(np.angle(w), 15, density=True)
+    density = np.ones_like(bins) / (2 * np.pi)
+    plt.plot(bins, density, linewidth=2, color='r')
+    plt.show()
+
+
+def block_assignment(wall, block, i, j):
+    """
+    Insert a `block` matrix to `wall` in position `i,j` with respect
+    to the blocks, i.e if `wall` has 4 block matrices of 2x2 then i,j runs through 0,1 and NOT
+    0,1,2,3.
+    """
+    # Validations
+    assert len(wall.shape) == 2
+    nd, nd2 = wall.shape
+    assert nd == nd2
+
+    assert len(block.shape) == 2
+    d, d2 = block.shape
+    assert d == d2
+
+    n = nd // d
+    assert n * d == nd
+
+    # Actual logic
+    wall[d * i: d * (i + 1), d * j:d * (j + 1)] = block
+
+
+def add_noise_to_matrix(H, d, p):
+    """
+    H - the matrix we want to add noise to
+
+    """
+    # Validations
+    assert len(H.shape) == 2
+    nd, nd2 = H.shape
+    assert nd == nd2
+    n = nd // d
+    assert n * d == nd
+
+    # Actual logic
+    index_list = []
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if 1 == np.random.binomial(1, 1 - p):
+                index_list.append((i, j))
+
+    for i, j in index_list:
+        block_assignment(H, truly_random_so_matrix(d), i, j)
---
+++ tests.py
@@ -0,0 +1,123 @@
+import pytest
+import numpy as np
+from matplotlib import pyplot as plt
+
+from exer1 import get_random_rotation, get_so_projection, get_error, solve_sync_with_spectral, truly_random_so_matrix, \
+    block_assignment, add_noise_to_matrix
+
+
+def test_rotation_matrix_sanity():
+    X = truly_random_so_matrix(3)
+    print(X.conj().T @ X)
+    assert np.allclose(X.conj().T @ X, np.eye(3), atol=1.e-8)
+    assert 1 == pytest.approx(np.linalg.det(X), 1e-5)
+
+
+def test_projection_matrix_for_rotation():
+    X = get_random_rotation()
+    projection = get_so_projection(X)
+    assert projection == pytest.approx(X, 1e-5)
+
+    assert np.eye(3) == pytest.approx(projection.T @ projection, 1e-5)
+    assert 1 == pytest.approx(np.linalg.det(projection), 1e-5)
+
+
+def test_projection_matrix_not_rotation():
+    X = np.array([[2, 0, 0], [0, 1, 0], [0, 0, 1]])
+    projection = get_so_projection(X)
+    assert projection == pytest.approx(np.eye(3), abs=1e-5)
+
+
+def test_error_sanity():
+    rot1 = get_random_rotation()
+    rot2 = get_random_rotation()
+
+    rot1_disturbed = rot1
+    rot2_disturbed = rot2
+    rot = np.array((rot1, rot2))
+    rot_dis = np.array((rot1_disturbed, rot2_disturbed))
+
+    assert 0 == pytest.approx(get_error(expected=rot, actual=rot_dis))
+
+
+def test_error_noisy():
+    rot1 = get_random_rotation()
+    rot2 = get_random_rotation()
+
+    rot1_disturbed = rot1 + np.random.normal(0, 0.1, (3, 3))
+    rot2_disturbed = rot2 + np.random.normal(0, 0.1, (3, 3))
+    rot = np.array((rot1, rot2))
+    rot_dis = np.array((rot1_disturbed, rot2_disturbed))
+
+    assert get_error(expected=rot, actual=rot_dis) < 0.4
+
+
+@pytest.mark.parametrize('times', range(100))
+def test_eigenvalue(times):
+    n = 5
+    d = 2
+    stack = [truly_random_so_matrix(d) for _ in range(n)]
+    V = np.vstack(stack)
+    B = V @ V.T.conj()
+
+    assert n * V == pytest.approx(B @ V)
+
+    R_hat = solve_sync_with_spectral(B, d)
+    V = V.reshape((n, d, d))
+    assert 0 == pytest.approx(get_error(R_hat, V, d), abs=1e-5)
+
+
+def test_histogram():
+    n = 400
+    d = 3
+    p = 0.5  # Probability of getting good matrix
+
+    stack = [truly_random_so_matrix(d) for _ in range(n)]
+    V = np.vstack(stack)
+    B = V @ V.T.conj()
+    B_noisy = np.copy(B)
+    add_noise_to_matrix(B_noisy, d, p)
+    W = B_noisy - p * (V @ V.T.conj())
+    v = np.linalg.eigvals(W)
+
+    plt.figure(2)
+
+    plt.hist(np.real(v), bins=30, label='real')
+    plt.hist(np.imag(v), bins=30, label='imaginary')
+
+    plt.title('Histograms of the spectrum matrix W.  n={n}, p={p}'.format(n=n, p=p))
+    plt.show()
+
+
+def test_histogram_B_noisy():
+    n = 600
+    d = 3
+    p = 0.04  # Probability of getting good matrix
+
+    stack = [truly_random_so_matrix(d) for _ in range(n)]
+    V = np.vstack(stack)
+    B = V @ V.T.conj()
+    B_noisy = np.copy(B)
+    add_noise_to_matrix(B_noisy, d, p)
+    v = np.linalg.eigvals(B_noisy)  # Note: B_noisy = W + \lambda u ut
+
+    plt.figure(2)
+
+    plt.hist(np.real(v), bins=30, label='real')
+    plt.hist(np.imag(v), bins=30, label='imaginary')
+
+    plt.title('Histograms of the spectrum matrix W.  n={n}, p={p}'.format(n=n, p=p))
+    plt.show()
+
+
+def test_block_assignment():
+    a = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
+    z = np.zeros((2, 2))
+    block_assignment(a, z, 1, 1)
+    print(a)
+
+
+def test_create_noisy_matrix():
+    a = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]], dtype='complex128')
+    add_noise_to_matrix(a, 2, 0.5)
+    print(a)
